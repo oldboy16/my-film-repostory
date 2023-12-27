0945aac968e7eaddf4352edc661a6663ed485de5
oldboy16 <hamidullayevmuhammadamin95gmail.com>
2023-12-27 23:10:07 +0500

    movie card

--- js/main.js
+++ js/main.js
@@ -17,43 +17,60 @@ window.addEventListener("scrollY", (a) => {
     }
 })
 
-// let partMovies = movies.slice(0, 21)  // slice ma'lumotni qirqib beradi
-// let elMovlist = document.querySelector(".movies__list")
 
-// partMovies.forEach((item) => {
-//     console.log(item);
-//     let newli = document.createElement("li")
-//     newli.innerHTML = `
-//     <div class="card card__item" style="width: 18rem;">
-//     <img src="https://i.ytimg.com/vi/${item.ytid}/hqdefault.jpg?" class="card-img-top" alt="...">
-//     <div class="card-body bg-black">
-//       <h5 class="card-title">${item.Title}</h5>
-//       <p class="card-text">${item.Categories}
-//       <p class="card-text">${item.movie_year}</p>
-//       <p class="card-text">${item.imdb_rating}</p>
-//       <a href="https://www.youtube.com/watch?v=${item.ytid}" class="btn btn-success target="_blank">Watch movie</a>
-//     </div>
-//     </div>
-//     `
-//     elMovlist.appendChild(newli)
-// });
-
-
-let partMovies = movies.slice(0,21)
+let partMovies = movies.slice(0,21)    //slice ni vazifasi qirqib olish
 let elMovlist = document.querySelector(".movies__list")
 
+fnRender(partMovies)
+function fnRender(data){
+    elMovlist.innerHTML = ''
+    data.forEach((item) => {
+        let newli = document.createElement("li")
+        newli.innerHTML = `
+        <div class="hero__card">
+        <img class="hero__img" src="https://i.ytimg.com/vi/${item.ytid}/hqdefault.jpg?" class="card-img-top" alt="...">
+        <p>${item.Categories}</p>
+        <h5 class="card__title">${item.Title}</h5>
+        <p class="card__year">${item.movie_year}-year</p>
+        <p class="card__rate">${item.imdb_rating} ⭐</p>
+        <a href="https://www.youtube.com/watch?v=${item.ytid}" class="btn btn-warning target="_blank">Watch movie</a>
+    </div>
+        `
+        elMovlist.appendChild(newli)
+    });
+}
 
-partMovies.forEach((item) => {
-    let newli = document.createElement("li")
-    newli.innerHTML = `
-    <div class="hero__card">
-    <img class="hero__img" src="https://i.ytimg.com/vi/${item.ytid}/hqdefault.jpg?" class="card-img-top" alt="...">
-    <p>${item.Categories}</p>
-    <h5 class="card__title">${item.Title}</h5>
-    <p class="card__year">${item.movie_year}-year</p>
-    <p class="card__rate">${item.imdb_rating}</p>
-    <a href="https://www.youtube.com/watch?v=${item.ytid}" class="btn btn-success target="_blank">Watch movie</a>
-</div>
-    `
-    elMovlist.appendChild(newli)
-});
+function fnYear(value){
+    if(value == "max"){
+        fnRender(partMovies.sort((a,b)=> b.movie_year - a.movie_year));
+    }else if(value == "old"){
+        fnRender(partMovies.sort((a,b)=> a.movie_year - b.movie_year));
+    }
+}
+
+function fnRanting(value){
+    if(value == "max"){
+        fnRender(partMovies.sort((a,b)=> b.imdb_rating - a.imdb_rating));
+    }else if(value == "min"){
+        fnRender(partMovies.sort((a,b)=> a.imdb_rating - b.imdb_rating));
+    }
+}
+
+let elSel = document.querySelector(".select__category")
+
+let arrCategory = []
+partMovies.forEach((item)=>{
+    if(!arrCategory.includes(item.Categories)){
+        arrCategory.push(item.Categories)
+    }
+})
+
+arrCategory.forEach((item)=>{
+    let newOption = document.createElement("option")
+    newOption.textContent = item
+    elSel.appendChild(newOption)
+})
+
+function fnCategory(value){
+    fnRender(partMovies.filter((item)=> item.Categories == value));
+}
